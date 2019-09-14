da4706511c9a6deac190f9c43a0fa720c3de0078
juliette ruchel <julietteru@gmail.com>
2019-09-14 15:16:14 -0300

    Added spaces to the writer class

--- TwoDrive.Domain/Writer.cs
+++ TwoDrive.Domain/Writer.cs
@@ -6,10 +6,15 @@ namespace TwoDrive.Domain
     public class Writer
     {
         public Guid Id { get; set; }
+
         public Guid Token { get; set; }
+
         public string UserName { get; set; }
+
         public string Password { get; set; }
+
         public ICollection<Writer> Friends { get; set; }
+        
         public ICollection<Claim> Claims { get; set; }
     }
 }
