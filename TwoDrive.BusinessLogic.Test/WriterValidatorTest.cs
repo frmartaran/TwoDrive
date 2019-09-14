696bda5bf704bb1f92414ab9767e5cc264e3c143
juliette ruchel <julietteru@gmail.com>
2019-09-14 16:41:04 -0300

    [RED] Created WriterValidator Class

---
+++ TwoDrive.BusinessLogic.Test/WriterValidatorTest.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwoDrive.Domain;
+
+namespace TwoDrive.BusinessLogic.Test
+{
+    [TestClass]
+    public class WriterValidatorTest
+    {
+        [TestMethod]
+        public void InvalidWriterNoToken()
+        {
+            var writer = new Writer{
+                UserName = "Writer",
+                Password = "A password",
+                Friends = new List<Writer>(),
+                Claims = new List<Claim>(),
+            };
+
+            var validator = new WriterValidator();
+            bool isValid = validator.isValid();
+
+            Assert.AreEqual(false, isValid);
+        }
+    }
+}
---
+++ TwoDrive.BusinessLogic/Validators/WriterValidator.cs
@@ -0,0 +1,14 @@
+using System;
+using TwoDrive.BusinessLogic.Interfaces;
+using TwoDrive.Domain;
+
+namespace TwoDrive.BusinessLogic.Validators
+{
+    public class WriterValidator : IValidator<Writer>
+    {
+        public bool isValid(Writer objectToValidate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
