62f7198d03ff95ceb4fd26bb384496ca89c53c38
juliette ruchel <julietteru@gmail.com>
2019-09-15 14:11:10 -0300

    [GREEN] InvalidFolderWithoutName

--- TwoDrive.BusinessLogic/Validators/ElementValidator.cs
+++ TwoDrive.BusinessLogic/Validators/ElementValidator.cs
@@ -6,8 +6,12 @@ namespace TwoDrive.BusinessLogic.Validators
 {
     public class ElementValidator : IValidator<Element>
     {
-        public bool isValid(Element objectToValidate)
+        public bool isValid(Element element)
         {
+            var hasName = !string.IsNullOrWhiteSpace(element.Name);
+            if (!hasName)
+                throw new ArgumentException("The folder or file should have a name");
+                
             return true;
         }
     }
