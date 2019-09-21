4ec4595abfaa0186b1bb01af51a1b4f1d57675e5
juliette Ruchel <julietteru@gmail.com>
2019-09-21 17:54:20 -0300

    [REFRACTOR] Moved dates to the element class

--- TwoDrive.Domain/Sesion.cs
+++ TwoDrive.Domain/Sesion.cs
@@ -3,6 +3,7 @@ namespace TwoDrive.Domain
 {
     public class Sesion
     {
+        public int Id { get; set; }
         public Guid Token { get; set; }
         public Writer Writer { get; set; }
     }
