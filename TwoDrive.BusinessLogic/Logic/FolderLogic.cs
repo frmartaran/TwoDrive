838b514cfaa34f00105e45cc14e714d7b234f62f
juliette Ruchel <julietteru@gmail.com>
2019-09-22 15:15:41 -0300

    Created Logic class for folder

---
+++ TwoDrive.BusinessLogic/Logic/FolderLogic.cs
@@ -0,0 +1,35 @@
+
+using System.Collections.Generic;
+using TwoDrive.BusinessLogic.Interfaces;
+using TwoDrive.Domain.FileManagement;
+
+namespace TwoDrive.BusinessLogic.Logic
+{
+    public class FolderLogic : ILogic<Folder>
+    {
+        public void Create(Folder folder)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(Folder folder)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Folder Get(int Id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ICollection<Folder> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Folder folder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
