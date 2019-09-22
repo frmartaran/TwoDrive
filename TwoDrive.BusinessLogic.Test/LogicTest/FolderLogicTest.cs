da9913ab07308fc3c59e6ce1a95fc448d99b9d82
juliette Ruchel <julietteru@gmail.com>
2019-09-22 16:10:13 -0300

    [RED] DeleteParentWithTwoChilds

--- TwoDrive.BusinessLogic.Test/LogicTest/FolderLogicTest.cs
+++ TwoDrive.BusinessLogic.Test/LogicTest/FolderLogicTest.cs
@@ -128,5 +128,51 @@ namespace TwoDrive.BusinessLogic.Test
             allFoldersInDb = repository.GetAll();
             Assert.AreEqual(0, allFoldersInDb.Count);            
         }
+
+        [TestMethod]
+        public void DeleteParentWithTwoChilds()
+        {
+            var context = ContextFactory.GetMemoryContext("Delete Test 2");
+            var repository = new ElementRepository(context);
+            repository.Insert(root);
+            repository.Save();
+            var child = new Folder
+            {
+                Id = 2,
+                CreationDate = new DateTime(2019, 9, 22),
+                DateModified = new DateTime(2019, 9, 22),
+                Name = "child",
+                Owner = root.Owner,
+                ParentFolder = root,
+                FolderChilden = new List<Element>()
+            };
+            var secondChild = new Folder
+            {
+                Id = 3,
+                CreationDate = new DateTime(2019, 9, 22),
+                DateModified = new DateTime(2019, 9, 22),
+                Name = "child 2",
+                Owner = root.Owner,
+                ParentFolder = root,
+                FolderChilden = new List<Element>()
+            };
+            root.FolderChilden.Add(child);
+            repository.Insert(child);
+            repository.Insert(secondChild);
+            repository.Update(root);
+            repository.Save();
+
+            var allFoldersInDb = repository.GetAll();
+            Assert.IsTrue(allFoldersInDb.Contains(root));
+            Assert.IsTrue(allFoldersInDb.Contains(child));
+            Assert.IsTrue(allFoldersInDb.Contains(secondChild));
+
+
+            var logic = new FolderLogic(repository);
+            logic.Delete(root);
+
+            allFoldersInDb = repository.GetAll();
+            Assert.AreEqual(0, allFoldersInDb.Count);            
+        }
     }
 }
