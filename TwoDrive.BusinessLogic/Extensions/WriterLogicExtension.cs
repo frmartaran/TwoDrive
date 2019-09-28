f01094a31e2befbe104b28f9af68c19cc132e72f
juliette Ruchel <julietteru@gmail.com>
2019-09-28 16:48:23 -0300

    [GREEN] WriterHasClaims, WriterDoesntHaveClaims

---
+++ TwoDrive.BusinessLogic/Extensions/WriterLogicExtension.cs
@@ -0,0 +1,18 @@
+
+using System.Linq;
+using TwoDrive.Domain;
+using TwoDrive.Domain.FileManagement;
+
+namespace TwoDrive.BusinessLogic.Extensions
+{
+    public static class WriterLogicExtension
+    {
+        public static bool HasClaimsFor(this Writer writer, Element element, ClaimType type)
+        {
+            return writer.Claims
+                    .Where(c => c.Element == element)
+                    .Where(c => c.Type == type)
+                    .Any();
+        }
+    }
+}
