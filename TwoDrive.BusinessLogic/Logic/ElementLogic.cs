ed164a7306fd5f817af05d3d7ae089ade887a106
frmartaran <frmartaran@gmail.com>
2019-10-04 22:37:22 -0300

    Constructor added to ElementLogic class

--- TwoDrive.BusinessLogic/Logic/ElementLogic.cs
+++ TwoDrive.BusinessLogic/Logic/ElementLogic.cs
@@ -1,10 +1,28 @@
 using TwoDrive.BusinessLogic.Interface;
+using TwoDrive.BusinessLogic.Interfaces;
+using TwoDrive.BusinessLogic.Interfaces.LogicInput;
+using TwoDrive.DataAccess.Interface;
 using TwoDrive.Domain.FileManagement;
 
 namespace TwoDrive.BusinessLogic
 {
     public class ElementLogic : IElementLogic
     {
+        private IFolderRepository FolderRepository { get; set; }
+
+        private IElementValidator ElementValidator { get; set; }
+
+        private IFileRepository FileRepository { get; set; }
+
+        public ElementLogic() { }
+
+        public ElementLogic(ElementLogicDependencies dependencies)
+        {
+            FolderRepository = dependencies.FolderRepository;
+            ElementValidator = dependencies.ElementValidator;
+            FileRepository = dependencies.FileRepository;
+        }
+
         public virtual void MoveElement(Element elementToMove, Folder elementDestination)
         {
             throw new System.NotImplementedException();
