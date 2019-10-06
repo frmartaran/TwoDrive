0d15ad7fc2044dd73f7512c90e7cf7823ad6cdc4
frmartaran <frmartaran@gmail.com>
2019-10-06 00:48:02 -0300

    [RED] folderAPI/Delete

---
+++ TwoDrive.WebAPI/Models/FileModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwoDrive.WebApi.Models
+{
+    public abstract class FileModel : ElementModel
+    {
+    }
+}
