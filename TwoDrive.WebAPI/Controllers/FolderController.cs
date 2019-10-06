25e063daad6475b2882ac20994b9dfa671a2937e
frmartaran <frmartaran@gmail.com>
2019-10-06 18:38:56 -0300

    [GREEN] MoveFolder

--- TwoDrive.WebAPI/Controllers/FolderController.cs
+++ TwoDrive.WebAPI/Controllers/FolderController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TwoDrive.BusinessLogic;
 using TwoDrive.BusinessLogic.Exceptions;
+using TwoDrive.BusinessLogic.Extensions;
 using TwoDrive.BusinessLogic.Interfaces;
+using TwoDrive.BusinessLogic.Interfaces.LogicInput;
+using TwoDrive.DataAccess.Interface;
 using TwoDrive.Domain;
+using TwoDrive.Domain.FileManagement;
 using TwoDrive.WebApi.Filters;
 
 namespace TwoDrive.WebApi.Controllers
@@ -12,9 +16,19 @@ namespace TwoDrive.WebApi.Controllers
     {
         private IFolderLogic FolderLogic { get; set; }
 
-        public FolderController(IFolderLogic folderLogic) : base()
+        private ISessionLogic Session { get; set; }
+
+        private IRepository<Element> ElementRepository { get; set; }
+
+        private IElementValidator Validator { get; set; }
+
+        public FolderController(IFolderLogic folderLogic, ISessionLogic session, 
+            IRepository<Element> elementRepository, IElementValidator validator) : base()
         {
             FolderLogic = folderLogic;
+            Session = session;
+            ElementRepository = elementRepository;
+            Validator = validator;
         }
 
         [HttpDelete("{id}")]
@@ -26,6 +40,33 @@ namespace TwoDrive.WebApi.Controllers
                 var folder = FolderLogic.Get(id);
                 FolderLogic.Delete(id);
                 return Ok($"Folder: {folder.Name} has been deleted");
+            }
+            catch (LogicException exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+        [HttpPost]
+        [ClaimFilter(ClaimType.Write)]
+        public IActionResult MoveFolder(int folderToMoveId, int folderDestinationId)
+        {
+            try
+            {
+                var token = HttpContext.Request.Headers["Authorization"];
+                var writer = Session.GetWriter(token);
+                var folderToMove = FolderLogic.Get(folderToMoveId);
+                var folderDestination = FolderLogic.Get(folderDestinationId);
+                if (FolderLogicExtension.IsWriterOwnerOfOriginAndDestination(writer, folderToMove, folderDestination))
+                {
+                    var moveElementDependencies = new MoveElementDependencies
+                    {
+                        ElementRepository = ElementRepository,
+                        ElementValidator = Validator
+                    };
+                    FolderLogic.MoveElement(folderToMove, folderDestination, moveElementDependencies);
+                }
+                return Ok($"Folder with id {folderToMoveId} was moved to destination with id {folderDestinationId}");
             }
             catch (LogicException exception)
             {
