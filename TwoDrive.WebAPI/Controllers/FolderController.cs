1a8bb4b33b4f7129b73414672ad7042db9c47707
frmartaran <frmartaran@gmail.com>
2019-10-06 16:01:48 -0300

    [WIP]

--- TwoDrive.WebAPI/Controllers/FolderController.cs
+++ TwoDrive.WebAPI/Controllers/FolderController.cs
@@ -1,11 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using TwoDrive.BusinessLogic;
+using TwoDrive.BusinessLogic.Interfaces;
+using TwoDrive.WebApi.Filters;
 
 namespace TwoDrive.WebApi.Controllers
 {
-    public class FolderController
+    [Route("api/[controller]")]
+    public class FolderController : ControllerBase
     {
+        private IFolderLogic FolderLogic { get; set; }
+
+        private ISessionLogic SessionLogic { get; set; }
+
+        public FolderController(IFolderLogic folderLogic, ISessionLogic sessions) : base()
+        {
+            FolderLogic = folderLogic;
+            SessionLogic = sessions;
+        }
+
+        [HttpDelete("{id}")]
+        [ClaimFilter("{id}", )]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                
+                var writer = Logic.Get(id);
+                var folder = FolderLogic.GetRootFolder(writer);
+                FolderLogic.Delete(folder.Id);
+                Logic.Delete(id);
+                return Ok($"Writer: {writer.UserName} has been deleted");
+            }
+            catch (LogicException exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
     }
 }
