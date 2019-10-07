68c8807b41f005a684fbcc0cbf2df3aa3fcc6285
juliette Ruchel <julietteru@gmail.com>
2019-10-07 00:37:55 -0300

    [RED] UpdateBadRequest

--- TwoDrive.WebApi.Test/FileControllerTest.cs
+++ TwoDrive.WebApi.Test/FileControllerTest.cs
@@ -631,5 +631,49 @@ namespace TwoDrive.WebApi.Test
             Assert.IsInstanceOfType(result, typeof(OkObjectResult));
             Assert.AreEqual("File Updated", okResult.Value);
         }
+
+        [TestMethod]
+        public void UpdateBadRequest()
+        {
+            var folder = new Folder
+            {
+                Id = 3,
+                Name = "Root",
+                FolderChildren = new List<Element>(),
+                Owner = new Writer()
+            };
+            var file = new TxtFile
+            {
+                Id = 1,
+                Name = "New file",
+                Content = "Content",
+                Owner = writer,
+                OwnerId = 2,
+                ParentFolder = folder,
+                ParentFolderId = 1,
+                CreationDate = new DateTime(2019, 6, 10),
+                DateModified = new DateTime(2019, 6, 10),
+            };
+            var model = new TxtModel().FromDomain(file) as TxtModel;
+            var mockLogic = new Mock<ILogic<File>>();
+            mockLogic.Setup(m => m.Update(It.IsAny<File>()))
+                .Throws(new ValidationException(""));
+            mockLogic.Setup(m => m.Get(It.IsAny<int>()))
+                .Returns<File>(null);
+
+            var mockModification = new Mock<IModificationLogic>();
+
+            var mockWriterLogic = new Mock<ILogic<Writer>>();
+            var mockFolderLogic = new Mock<IFolderLogic>();
+            var mockSession = new Mock<ICurrent>();
+
+            var controller = new FileController(mockLogic.Object, mockFolderLogic.Object,
+                mockWriterLogic.Object, mockSession.Object, mockModification.Object);
+            var result = controller.Update(1, model);
+            var okResult = result as BadRequestObjectResult;
+
+            mockLogic.VerifyAll();
+            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+        }
     }
 }
