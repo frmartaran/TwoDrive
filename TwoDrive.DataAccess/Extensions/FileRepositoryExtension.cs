2dd9ec4a8f5ecbfda03733cf68a25c185164a9c5
frmartaran <frmartaran@gmail.com>
2019-10-08 09:44:45 -0300

    [GREEN] FileRepository GetAll

---
+++ TwoDrive.DataAccess/Extensions/FileRepositoryExtension.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TwoDrive.DataAccess.Interface.LogicInput;
+using TwoDrive.Domain.FileManagement;
+
+namespace TwoDrive.DataAccess.Extensions
+{
+    public static class FileRepositoryExtension
+    {
+        public static List<File> OrderByDescending(FileFilter filter, IQueryable<File> filesFilteredByName)
+        {
+            IOrderedQueryable<File> orderIntermediateResult = new EnumerableQuery<File>(filesFilteredByName);
+            if (filter.IsOrderByName)
+            {
+                orderIntermediateResult = filesFilteredByName.OrderByDescending(f => f.Name);
+            }
+            if (filter.IsOrderByModificationDate)
+            {
+                orderIntermediateResult = !filter.IsOrderByName
+                    ? orderIntermediateResult.OrderByDescending(f => f.DateModified)
+                    : orderIntermediateResult.ThenByDescending(f => f.DateModified);
+            }
+            if (filter.IsOrderByCreationDate)
+            {
+                orderIntermediateResult = !(filter.IsOrderByName || filter.IsOrderByModificationDate)
+                    ? orderIntermediateResult.OrderByDescending(f => f.CreationDate)
+                    : orderIntermediateResult.ThenByDescending(f => f.CreationDate);
+            }
+            return orderIntermediateResult.ToList();
+        }
+
+        public static List<File> OrderBy(FileFilter filter, IQueryable<File> filesFilteredByName)
+        {
+            IOrderedQueryable<File> orderIntermediateResult = new EnumerableQuery<File>(filesFilteredByName);
+            if (filter.IsOrderByName)
+            {
+                orderIntermediateResult = filesFilteredByName.OrderBy(f => f.Name);
+            }
+            if (filter.IsOrderByModificationDate)
+            {
+                orderIntermediateResult = !filter.IsOrderByName
+                    ? orderIntermediateResult.OrderBy(f => f.DateModified)
+                    : orderIntermediateResult.ThenBy(f => f.DateModified);
+            }
+            if (filter.IsOrderByCreationDate)
+            {
+                orderIntermediateResult = !(filter.IsOrderByName || filter.IsOrderByModificationDate)
+                    ? orderIntermediateResult.OrderBy(f => f.CreationDate)
+                    : orderIntermediateResult.ThenBy(f => f.CreationDate);
+            }
+            return orderIntermediateResult.ToList();
+        }
+    }
+}
--- TwoDrive.DataAccess/Repositories/FileRepository.cs
+++ TwoDrive.DataAccess/Repositories/FileRepository.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using System.Linq;
+using TwoDrive.DataAccess.Extensions;
 using TwoDrive.DataAccess.Interface;
+using TwoDrive.DataAccess.Interface.LogicInput;
 using TwoDrive.Domain.FileManagement;
 
 namespace TwoDrive.DataAccess
@@ -16,6 +19,23 @@ namespace TwoDrive.DataAccess
             return table.Where(e => e.Name == file.Name)
                         .Where(e => e.ParentFolderId == file.ParentFolderId)
                         .Any();
+        }
+
+        public ICollection<File> GetAll(FileFilter filter)
+        {
+            var filesToReturn = new List<File>();
+            var filesFilteredByName = filter.Id.HasValue
+                ? table.Where(e => e.Name.Contains(filter.Name) && e.Owner.Id == filter.Id.Value)
+                : table.Where(e => e.Name.Contains(filter.Name));
+
+            if (filter.IsOrderDescending)
+            {
+                return FileRepositoryExtension.OrderByDescending(filter, filesFilteredByName);
+            }
+            else
+            {
+                return FileRepositoryExtension.OrderBy(filter, filesFilteredByName);
+            }
         }
     }
 }
