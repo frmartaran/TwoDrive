5018474decd36e74bc6b7901d5f37f9010f8ebf7
juliette Ruchel <julietteru@gmail.com>
2019-10-19 18:59:08 -0300

    [RED] FileNotFound

--- TwoDrive.Formatter.Test/XMLFormatterTest.cs
+++ TwoDrive.Formatter.Test/XMLFormatterTest.cs
@@ -1,10 +1,12 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.IO;
 using System.Xml;
 using TwoDrive.BusinessLogic.Interfaces;
 using TwoDrive.Domain.FileManagement;
 using TwoDrive.Formatter.Interface;
 using TwoDrive.Formatters;
+using TwoDrive.Formatters.Exceptions;
 
 namespace TwoDrive.Formatter.Test
 {
@@ -22,6 +24,16 @@ namespace TwoDrive.Formatter.Test
             var formatter = new XMLFormatter(mockFolderLogic.Object);
             var document = formatter.Load<XmlDocument>(path);
             Assert.IsNotNull(document);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatterException))]
+        public void FileNotFound()
+        {
+            var path = "path";
+            var mockFolderLogic = new Mock<IFolderLogic>();
+            var formatter = new XMLFormatter(mockFolderLogic.Object);
+            var document = formatter.Load<XmlDocument>(path);
         }
     }
 }
---
+++ TwoDrive.Formatters/Exceptions/FormatterException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoDrive.Formatters.Exceptions
+{
+    public class FormatterException : Exception
+    {
+        public FormatterException(string message) : base(message) { }
+
+        public FormatterException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
