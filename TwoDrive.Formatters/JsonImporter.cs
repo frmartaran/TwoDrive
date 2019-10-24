e96c4a25b3f1ee876bbe00084b74b31e9b5ac5db
juliette Ruchel <julietteru@gmail.com>
2019-10-24 17:16:43 -0300

    [GREEN] SaveAFolder

--- TwoDrive.Formatters/JsonImporter.cs
+++ TwoDrive.Formatters/JsonImporter.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using TwoDrive.Importer.Interface;
 using TwoDrive.Importer.Interface.IFileManagement;
+using TwoDrive.Importer.MockDomain;
 using TwoDrive.Importers.Exceptions;
 
 namespace TwoDrive.Importer
@@ -21,7 +23,9 @@ namespace TwoDrive.Importer
 
         public IFolder Import(string path)
         {
-            throw new NotImplementedException();
+            var jsonString = Load<string>(path);
+            var folder = JsonConvert.DeserializeObject<MockFolder>(jsonString);
+            return folder;
         }
 
         public T Load<T>(string path) where T : class
