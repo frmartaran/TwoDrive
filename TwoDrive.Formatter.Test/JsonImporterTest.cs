012599914fa1922ee5ec200cb2fb5d12b110187c
juliette Ruchel <julietteru@gmail.com>
2019-10-24 17:11:33 -0300

    [RED] SaveAFolder

--- TwoDrive.Formatter.Test/JsonImporterTest.cs
+++ TwoDrive.Formatter.Test/JsonImporterTest.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TwoDrive.Importer.Interface.IFileManagement;
 using TwoDrive.Importers.Exceptions;
 
 namespace TwoDrive.Importer.Test
@@ -28,6 +29,17 @@ namespace TwoDrive.Importer.Test
             var path = $"{examplesRoot}\\doc.json";
             var importer = new JsonImporter();
             var jsonFile = importer.Load<string>(path);
+        }
+
+        [TestMethod]
+        public void SaveAFolder()
+        {
+            var path = $"{examplesRoot}\\document.json";
+            var importer = new JsonImporter();
+            var folder = importer.Import(path);
+
+            Assert.IsNotNull(folder);
+            Assert.IsInstanceOfType(folder, typeof(IFolder));
         }
     }
 }
