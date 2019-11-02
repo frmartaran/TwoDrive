5ed7a2d2f4390a06f447ffd293fd4f16c4b5431d
juliette Ruchel <julietteru@gmail.com>
2019-11-02 16:06:53 -0300

    [RED] ImportFolderWithAFile

--- TwoDrive.BusinessLogic.Test/LogicTest/ImporterLogicTest.cs
+++ TwoDrive.BusinessLogic.Test/LogicTest/ImporterLogicTest.cs
@@ -171,6 +171,48 @@ namespace TwoDrive.BusinessLogic.Test.LogicTest
             
         }
 
+        [TestMethod]
+        public void ImportAFolderWithAFile()
+        {
+            var context = ContextFactory.GetMemoryContext("Import A Folder with A File");
+            var folderRepository = new FolderRepository(context);
+            var fileRepository = new FileRepository(context);
+            var fileValidator = new Mock<IValidator<Element>>().Object;
+            var validator = new Mock<IFolderValidator>().Object;
+            var modificationRepository = new ModificationRepository(context);
+            var modificationsLogic = new ModificationLogic(modificationRepository);
+            var folderDependecies = new ElementLogicDependencies(folderRepository, fileRepository,
+                validator, modificationRepository);
+            var writerRepository = new WriterRepository(context);
+            var writerValidator = new Mock<IValidator<Writer>>().Object;
+
+            var folderLogic = new FolderLogic(folderDependecies);
+            var fileLogic = new FileLogic(fileRepository, fileValidator);
+            var writerLogic = new WriterLogic(writerRepository, writerValidator);
+            var importerDependecies = new ImporterLogicDependencies(folderLogic, fileLogic, writerLogic,
+                modificationsLogic);
+            var options = new ImportingOptions
+            {
+                FilePath = $"{examplesRootForXML}\\Simple Tree With File.xml",
+                FileType = "XML",
+                Owner = writer
+            };
+            writerRepository.Insert(writer);
+            writerRepository.Save();
+
+            var importerLogic = new ImporterLogic(options, importerDependecies);
+            importerLogic.Import();
+
+            var foldersInDb = folderLogic.GetAll();
+            var claims = writer.Claims.Count;
+            var modificationsCount = context.Modifications.Count();
+            var filesCount = context.Files.Count();
+            Assert.AreEqual(2, foldersInDb.Count);
+            Assert.AreEqual(7, claims);
+            Assert.AreEqual(3, modificationsCount);
+
+        }
+
 
     }
 }
